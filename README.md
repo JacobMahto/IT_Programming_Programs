2fb47b07841458003e5339200ea3211236d31a4e
jvm <jacobmahto148@gmail.com>
2019-01-11 00:50:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# IT_Programming_Programs
---
+++ swapByReference_Prog1.cpp
@@ -0,0 +1,21 @@
+/* 
+ * Program : Swap by Reference
+ * Description : 
+ * Interaction : User Input
+ * Author : J. Mahto (@jvm)
+ * Date : 
+ * Environment : Netbeans 8.2 C++
+ * Copyright : Moral Knowledge Public Sr. Sec. School
+ * Note : For use in Turbo C++ 
+ * 
+ */
+
+#include<iostream>
+#include<stdio.h>
+#include "conio.h"
+
+using namespace std;
+int main(){
+    cout<<"Hello";
+    getchar();
+}
